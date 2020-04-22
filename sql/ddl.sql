66b87cda4dc93d6a694eab85a27233eb615a7fdd
Xiaoguang Sun <sunxiaoguang@zhihu.com>
2020-04-22 15:23:59 +0800

    Add data model graph

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -2,8 +2,8 @@ CREATE TABLE object_type (
   id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   name VARCHAR(64) NOT NULL,
   description VARCHAR(512),
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   UNIQUE KEY(name)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -13,8 +13,8 @@ CREATE TABLE object_status (
   type_id SMALLINT UNSIGNED NOT NULL,
   name VARCHAR(64),
   description VARCHAR(512),
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   UNIQUE KEY(type_id, name)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -24,8 +24,8 @@ CREATE TABLE object_state (
   status_id SMALLINT UNSIGNED NOT NULL,
   name VARCHAR(64),
   description VARCHAR(512),
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   UNIQUE KEY(status_id, name)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -39,9 +39,9 @@ CREATE TABLE object (
   description VARCHAR(1024),
   status_id SMALLINT UNSIGNED NOT NULL,
   state_id SMALLINT UNSIGNED NOT NULL,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   UNIQUE KEY(type_id, name),
   INDEX(delete_time, id)
@@ -56,9 +56,9 @@ CREATE TABLE deleted_object (
   description VARCHAR(1024),
   status_id SMALLINT UNSIGNED NOT NULL,
   state_id SMALLINT UNSIGNED NOT NULL,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   INDEX(type_id, delete_time)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -69,8 +69,8 @@ CREATE TABLE object_meta (
   name VARCHAR(256),
   value_type TINYINT UNSIGNED NOT NULL DEFAULT 1 COMMENT '1: STRING 2: INTEGER, 3: DOUBLE, 4: BOOLEAN',
   description VARCHAR(1024),
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   UNIQUE KEY(type_id, name)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -79,9 +79,9 @@ CREATE TABLE object_meta_value (
   object_id BIGINT UNSIGNED NOT NULL,
   meta_id MEDIUMINT UNSIGNED NOT NULL,
   value TEXT,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(object_id, meta_id),
   INDEX(delete_time, object_id, meta_id)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -90,9 +90,9 @@ CREATE TABLE deleted_object_meta_value (
   object_id BIGINT UNSIGNED NOT NULL,
   meta_id MEDIUMINT UNSIGNED NOT NULL,
   value TEXT,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(object_id, meta_id),
   INDEX(delete_time, object_id)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -104,9 +104,9 @@ CREATE TABLE object_log (
   format TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0: text/plain 1: application/json',
   source TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0: INTERNAL 1: API 2: USER 3: SYSTEM',
   message TEXT,
-  create_by varchar(255) NOT NULL DEFAULT '',
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_by VARCHAR(255) NOT NULL DEFAULT '',
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   INDEX(object_id, create_time),
   INDEX(delete_time, id)
@@ -119,9 +119,9 @@ CREATE TABLE deleted_object_log (
   format TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0: text/plain 1: application/json',
   source TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0: INTERNAL 1: API 2: USER 3: SYSTEM',
   message TEXT,
-  create_by varchar(255) NOT NULL DEFAULT '',
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_by VARCHAR(255) NOT NULL DEFAULT '',
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   INDEX(object_id, delete_time, id)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -132,9 +132,9 @@ CREATE TABLE object_relation_type (
   to_type_id SMALLINT UNSIGNED NOT NULL,
   name VARCHAR(64) NOT NULL,
   description VARCHAR(512),
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   UNIQUE KEY(from_type_id, to_type_id, name),
   INDEX(delete_time, id)
@@ -146,9 +146,9 @@ CREATE TABLE deleted_object_relation_type (
   to_type_id SMALLINT UNSIGNED NOT NULL,
   name VARCHAR(64) NOT NULL,
   description VARCHAR(512),
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
   INDEX(from_type_id, delete_time)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
@@ -157,9 +157,9 @@ CREATE TABLE object_relation (
   from_object_id BIGINT UNSIGNED NOT NULL,
   relation_type_id MEDIUMINT UNSIGNED NOT NULL,
   to_object_id BIGINT UNSIGNED NOT NULL,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(from_object_id, relation_type_id, to_object_id),
   INDEX(from_object_id, relation_type_id, update_time),
   INDEX(from_object_id, update_time),
@@ -170,47 +170,47 @@ CREATE TABLE deleted_object_relation (
   from_object_id BIGINT UNSIGNED NOT NULL,
   relation_type_id MEDIUMINT UNSIGNED NOT NULL,
   to_object_id BIGINT UNSIGNED NOT NULL,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(from_object_id, relation_type_id, to_object_id),
   INDEX(from_object_id, delete_time)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE object_relation_meta (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  type_id MEDIUMINT UNSIGNED NOT NULL,
+  relation_type_id MEDIUMINT UNSIGNED NOT NULL,
   name VARCHAR(256),
   value_type TINYINT UNSIGNED NOT NULL DEFAULT 1 COMMENT '1: STRING 2: INTEGER, 3: DOUBLE, 4: BOOLEAN',
   description VARCHAR(1024),
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  delete_time timestamp NULL DEFAULT NULL,
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY(id),
-  UNIQUE KEY(type_id, name)
+  UNIQUE KEY(relation_type_id, name)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE object_relation_meta_value (
   from_object_id BIGINT UNSIGNED NOT NULL,
-  relation_id MEDIUMINT UNSIGNED NOT NULL,
+  relation_type_id MEDIUMINT UNSIGNED NOT NULL,
   to_object_id BIGINT UNSIGNED NOT NULL,
   meta_id INT UNSIGNED NOT NULL,
   value TEXT,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
-  PRIMARY KEY(from_object_id, relation_id, to_object_id, meta_id),
-  INDEX(delete_time, from_object_id, relation_id, to_object_id, meta_id)
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY(from_object_id, relation_type_id, to_object_id, meta_id),
+  INDEX(delete_time, from_object_id, relation_type_id, to_object_id, meta_id)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE deleted_object_relation_meta_value (
   from_object_id BIGINT UNSIGNED NOT NULL,
-  relation_id MEDIUMINT UNSIGNED NOT NULL,
+  relation_type_id MEDIUMINT UNSIGNED NOT NULL,
   to_object_id BIGINT UNSIGNED NOT NULL,
   meta_id INT UNSIGNED NOT NULL,
   value TEXT,
-  create_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_time timestamp NULL DEFAULT NULL,
-  delete_time timestamp NULL DEFAULT NULL,
-  PRIMARY KEY(from_object_id, relation_id, to_object_id, meta_id),
-  INDEX(delete_time, from_object_id, relation_id, to_object_id)
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  update_time TIMESTAMP NULL DEFAULT NULL,
+  delete_time TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY(from_object_id, relation_type_id, to_object_id, meta_id),
+  INDEX(delete_time, from_object_id, relation_type_id, to_object_id)
 ) ENGINE=InnoDB CHARSET utf8mb4 COLLATE=utf8mb4_bin;
